72f475456da593344ad576ceafad9d738d907de3
Aman Bhatt <aman.bhatt@AMAN-BHATT-MAC.local>
2023-10-13 18:21:07 +0530

    added apis

--- chatapp/main.py
+++ chatapp/main.py
@@ -5,28 +5,166 @@ import redis
 import json
 import datetime
 import pytz
+import psycopg2
+import pandas.io.sql as psql
+import requests
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your own secret key
-# socketio = SocketIO(app)
 
 redis_db = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @app.route('/')
 def chat():
-    # return render_template('chat.html')
     print("helllo /mainpage called")
     return "<h1>hi</h1>"
 
-# @socketio.on('send_message')
-# def handle_message(data):
-#     message = data['message']
-#     socketio.emit('message', {'message': message}, broadcast=True)
+def connect(query, insert_data=None):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # Establish a connection to the PostgreSQL database
+        conn = psycopg2.connect(
+            host="localhost",
+            database="postgres",
+            port=5433,
+            user="postgres",
+            password="postgres"
+        )
+
+        # Create a cursor
+        cursor = conn.cursor()
+
+        if insert_data:
+            # If insert_data is provided, execute an INSERT query
+            insert_query = "INSERT INTO perz (userid, message, perz_tags) VALUES (%s, %s, %s)"
+            cursor.execute(insert_query, (insert_data['userid'], insert_data['message'], insert_data['perz_tags']))
+            conn.commit()
+        else:
+            # If insert_data is not provided, execute the SELECT query
+            dataframe = psql.read_sql(query, conn)
+            return dataframe
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error:", error)
+
+    finally:
+        if conn:
+            conn.close()
+            print('Database connection closed.')
+
+
+# insert_data = {
+#     'userid': 'Some Value',
+#     'message': '42',
+#     'perz_tags': 'Another Value'
+# }
+# df = connect(None,insert_data)
+# df2= connect("select * from perz", None)
+
+def hit_chat_completions_api(content):
+    # API endpoint URL
+    api_url = "http://10.5.10.104:10101/openai/chat/completions"
+
+    # JSON data to send in the POST request
+    json_data = {
+        "username": "sa_perz",
+        "password": "redBus@365",
+        "api": 1,
+        "request": {
+            "messages": [
+                {
+                    "role": "system",
+                    "content": "You are a summarisation assistant."
+                },
+                {
+                    "role": "user",
+                    "content": content
+                },
+                {
+                    "role": "user",
+                    "content": "Understand the context of it."
+                },
+                {
+                    "role": "user",
+                    "content": "summarise journey experience of user in bus in 2-3 keywords and return only keywords as response related to bus"
+                }
+            ]
+        }
+    }
+
+    try:
+        # Send an HTTP POST request
+        response = requests.post(api_url, json=json_data)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse and print the response content (assuming it's in JSON format)
+            response_data = response.json()
+            print("Response:", response_data)
+            return response_data
+        else:
+            print("HTTP request failed with status code:", response.status_code)
+
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+
+
+
+userids_array = ["11","12","13","14","15"]
+
+def get_user_messages(data):
+
+    for userid in userids_array:
+
+        target_userid = userid
+        messages_by_userid = []
+
+        for key, value in data.items():
+            message_data = json.loads(value)
+            if message_data.get("userid") == target_userid:
+                messages_by_userid.append(message_data["message"])
+
+        print("userid: "+userid+" = "+str(messages_by_userid))
+        response_data = hit_chat_completions_api(str(messages_by_userid))
+        content = response_data['response']['openAIResponse']['choices'][0]['message']['content']
+
+    
+
+
+        
+
+
+
+
+    
+
+
+
+
+
+@app.route('/clear_session',methods=['GET'])
+def clear_session():
+    print("here in clear session")
+
+    keys_and_values = {}
+
+    for key in redis_db.scan_iter("*"):
+        value = redis_db.get(key)
+        keys_and_values[key.decode('utf-8')] = value.decode('utf-8')
+    get_user_messages(keys_and_values)
+
+    df= connect(" Insert into perz values (1,'hello','clean') ")
+    print(df)
+    print("helllo /clear_session called")
+    return "<h1>session cleared</h1>"
+
+
+
 
 @app.route('/get_all_users', methods=['GET'])
 def get_all_users():
-    array = ["11","12","13","14","15"]
-    return jsonify(array)
+   
+    return jsonify(userids_array)
 
 
 # {"message": "Hello, sdfsdfsdfsdf!",
@@ -65,7 +203,7 @@ def read_messages():
 
     for key in redis_db.scan_iter("*"):
         value = redis_db.get(key)
-        keys_and_values[key.decode('utf-8')] = value.decode('utf-8')
+        keys_and_values[key.decode('utf-8')] = json.loads( value.decode('utf-8') )
 
     sorted_data = dict( sorted(keys_and_values.items(), key=lambda item: int(item[0])) )
 
